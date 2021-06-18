4c9be04da4b6ddf77e74686abfb117f40c7e5e42
ContatoViniciusGuanabara <contatoviniciusguanabara@gmail.com>
2021-06-17 23:14:51 -0300

    testando rotas
    teste de rotas para desenvolver o CRUD

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.json())
 
+app.get('/hello_word', (req, res) => {
+    res.send('Hello word!')
+})
+
 app.listen(5000, () => {
     console.log('Server running on port: ' + 5000)
 })
